01edf8ab33be952e13ad2f9c8c89a11ace38b641
Furkan Kazaz <furkan.kazaz@telpa.entp>
2019-06-28 14:14:26 +0300

    Added new button design to face recognation layout

--- app/src/main/java/com/example/gm_face_ai/MainActivity.java
+++ app/src/main/java/com/example/gm_face_ai/MainActivity.java
@@ -18,8 +18,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         TextView txtInfo= findViewById(R.id.textInfo);
         txtInfo.setText("GENERAL MOBİLE AR-GE MERKEZİNDE Mert Gölcü Tarafından Oluşturuldu. \n Daha Fazla Bilgi için : https://github.com/mgolcu00/GM-Face-AI ");
+
+
 
 
         Button btnFaceDet = findViewById(R.id.btnFaceDet);
