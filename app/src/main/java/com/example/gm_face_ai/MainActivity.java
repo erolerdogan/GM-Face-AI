bc33e5e231f8ab2610c124fe0896d0941b221dd8
mgolcu00 <mgolcu00@gmail.com>
2019-06-28 16:23:05 +0300

    Landmark button snackbar added.

--- app/src/main/java/com/example/gm_face_ai/MainActivity.java
+++ app/src/main/java/com/example/gm_face_ai/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.gm_face_ai;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.example.gm_face_ai.FaceDetection.FaceTrackerActivity;
 import com.example.gm_face_ai.FaceSpotter.FaceActivity;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +23,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView txtInfo= findViewById(R.id.textInfo);
         txtInfo.setText("GENERAL MOBİLE AR-GE MERKEZİNDE GM Çalışanları Tarafından Oluşturuldu. \n Daha Fazla Bilgi için : https://github.com/general-mobile ");
-
+        Snackbar sb;
+        ConstraintLayout container = findViewById(R.id.container2);
+        sb =  Snackbar.make(container, "Bu Hizmet şuan da geliştiriliyor...", Snackbar.LENGTH_INDEFINITE);
 
         Button btnFaceDet = findViewById(R.id.btnFaceDet);
         btnFaceDet.setOnClickListener(new View.OnClickListener() {
@@ -49,6 +54,21 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("TAG: ","Button Clicked");
                 Intent intent = new Intent(getApplicationContext(), com.example.gm_face_ai.FaceRecognizer.MainActivity.class);
                 startActivity(intent);
+
+            }
+        });
+
+        Button btnLandmark = findViewById(R.id.btnFaceDet4);
+        btnLandmark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sb.setAction("OK", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        sb.dismiss();
+                    }
+                });
+                sb.show();
 
             }
         });
